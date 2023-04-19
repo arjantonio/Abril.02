66d231283e0f75ad890a53b4d0072020d2467848
arjantonio <arjantonio@hotmail.com>
2023-04-19 12:27:14 -0500

    Comentarios

--- Lab02.R
+++ Lab02.R
@@ -2,5 +2,8 @@ html <- GET("https://www.mediawiki.org/wiki/MediaWiki")
 content <- content(html, as = "text")
 parsedHtml <- htmlParse(content)
 xpath <- "//title"
+# Utilizando la función: xpathSApply()
 Titulos <- xpathSApply(parsedHtml, xpath, xmlValue)
+# Se muestra el Título de la Pagina Web
+cat("Título de la Página Web:\n")
 cat(titulos, sep = "\n")
