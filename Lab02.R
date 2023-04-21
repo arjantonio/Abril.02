97a20ec21400edd812e4e3d90895b1623f93c6b0
melissayauri <yaurimelissa@gmail.ccom>
2023-04-21 09:54:11 -0500

    Resolviendo la pregunta 1.3

--- Lab02.R
+++ Lab02.R
@@ -1,9 +1,37 @@
+library(httr)
+library(XML)
+
+# Pregunta 1.1 
+# Almacenando la URl 
 html <- GET("https://www.mediawiki.org/wiki/MediaWiki")
-content <- content(html, as = "text")
-parsedHtml <- htmlParse(content)
+contenido <- content(html, as = "text")
+parsedHtml <- htmlParse(contenido)
+
+# Pregunta 1.2
+# Obteniendo el título de la web
 xpath <- "//title"
 # Utilizando la función: xpathSApply()
-Titulos <- xpathSApply(parsedHtml, xpath, xmlValue)
+titulos <- xpathSApply(parsedHtml, xpath, xmlValue)
 # Se muestra el Título de la Pagina Web
 cat("Título de la Página Web:\n")
 cat(titulos, sep = "\n")
+
+# Pregunta 1.3 
+
+# Extrayendo los nombres de la etiqueta <a></a>
+name_hyperlink <- xpathSApply(parsedHtml, "//a", xmlValue)
+#name_hyperlink 
+# Extrayendo los valores del atributo href de la etiqueta <a></>
+url_hyperlink <- xpathSApply(parsedHtml, "//a", xmlGetAttr, "href")
+#url_hyperlink
+# Validando valores null
+
+# Validando si los nombres de la etiqueta y los valores del atributo href tienen los valores null
+null_name <- sapply(name_hyperlink, is.null)
+null_href <- sapply(url_hyperlink, is.null)
+# Reemplazando los valores null por el valor NA
+name_hyperlink[null_name] <- NA
+url_hyperlink[null_href] <- NA
+
+name_hyperlink <- unlist(name_hyperlink)
+url_hyperlink  <- unlist(url_hyperlink)
